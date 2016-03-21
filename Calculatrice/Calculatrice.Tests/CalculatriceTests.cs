9a32e36f70fde17052d78726c47f926200672a92
Anthaax <guillaume.fimes@laposte.net>
2016-03-21 15:57:22 +0100

    Création de Calculatrice et Calculatrice.Tests

---
+++ Calculatrice/Calculatrice.Tests/CalculatriceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculatrice.Tests
+{
+    [TestFixture]
+    public class CalculatriceTests
+    {
+        [Test]
+        public void AdditionTest()
+        {
+            Calculatrice c = new Calculatrice();
+            Assert.AreEqual(2, c.Addition(1, 1));
+
+        }
+    }
+}
---
+++ Calculatrice/Calculatrice/Calculatrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice
+{
+    public class Calculatrice
+    {
+        public Calculatrice()
+        {
+
+        }
+
+        public int Addition(int numberA, int numberB)
+        {
+            return numberA + numberB;
+        }
+    }
+}
