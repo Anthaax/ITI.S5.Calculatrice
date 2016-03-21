6d85d282c3dd5c97ae31995cb16252ba3d72f919
Anthaax <guillaume.fimes@laposte.net>
2016-03-21 16:50:58 +0100

    Build.cs

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -119,13 +119,13 @@ namespace CodeCake
                     // AND CodeCakeBuilder is running in interactive mode (ie. no -nointeraction parameter),
                     // then the user is prompted to enter it.
                     // This is specific to CodeCake (in Code.Cake.dll).
-                    var apiKey = Cake.InteractiveEnvironmentVariable("NUGET_API_KEY");
-                   if (string.IsNullOrEmpty(apiKey)) throw new InvalidOperationException("Could not resolve NuGet API key.");
+                    //var apiKey = Cake.InteractiveEnvironmentVariable("NUGET_API_KEY");
+                   //if (string.IsNullOrEmpty(apiKey)) throw new InvalidOperationException("Could not resolve NuGet API key.");
 
                    var settings = new NuGetPushSettings
                    {
                        Source = "https://www.myget.org/feed/Packages/calculatricecake",
-                       ApiKey = apiKey
+                      // ApiKey = apiKey
                    };
 
                    foreach (var nupkg in Cake.GetFiles(releasesDir.Path + "/*.nupkg"))
